6d98340b4b1c7913c3fa6688cd21e6fdcaba88dd
Umar6501 <136899700+Umar6501@users.noreply.github.com>
2023-10-09 23:45:55 +0500

    homework10

--- src/Pages/SingleProduct/Details.jsx
+++ src/Pages/SingleProduct/Details.jsx
@@ -1,38 +1,43 @@
-// import React, { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-// const Details = () => {
-//   const { id } = useParams();
-//   const [product, setProduct] = useState(null);
+const Details = () => {
+  const param = useParams();
+  const [product, setProduct] = useState([]);
+  // console.log(param.id * 1);
+  const paramId = param.id * 1;
+  // console.log(paramId);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/products/" + paramId
+        );
+        setProduct([response.data]);
+        // console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get(
-//           "http://localhost:3000/products/" + id
-//         );
-//         setProduct(response.data);
-//         console.log(response.data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
+    fetchData();
+  }, [paramId]);
 
-//     fetchData();
-//   }, [id]);
+  if (!product) {
+    return <div>Loading...</div>;
+  }
 
-//   if (!product) {
-//     return <div>Loading...</div>;
-//   }
+  return (
+    <div>
+      {product.map((el) => {
+        <div key={el.id}>
+          <h1>{el.category}</h1>
+        </div>;
+      })}
+      {/* Отобразите остальные свойства продукта */}
+    </div>
+  );
+};
 
-//   return (
-//     <div>
-//       <h2>{news.name}</h2>
-//       <p>{product.description}</p>
-//       {/* Отобразите остальные свойства продукта */}
-//     </div>
-//   );
-// };
-
-// export default Details;
+export default Details;
