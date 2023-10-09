70040b64af936b72c2f6cf107fd68cf1fabb60c0
Umar6501 <136899700+Umar6501@users.noreply.github.com>
2023-10-09 18:07:36 +0500

    homework10

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { Header } from "react-bootstrap/lib/Modal";
+import React from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+const Header = React.lazy(() => import("./components/Header/Header"));
+const Shop = React.lazy(() => import("./Pages/Shop/Shop"));
+const AllProducs = React.lazy(() => import("./Pages/AllProducs/AllProducs"));
 
 function App() {
   return (
     <div>
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          {/* //birinchi project ↓ */}
+          {/* <Route path="/" element={<AllProducs />}></Route> */}
+          {/* ikkinchi project ↓ */}
+          <Route path="/Shop" element={<Shop />}></Route>
+          {/* uchinchi project ↓ */}
+          {/* <Route path="/______" element={<_______ />}></Route> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
