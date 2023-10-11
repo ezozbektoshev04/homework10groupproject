3960d31f51498991d212c01a9bd90ac7825de672
Yusupov Elbek <elbeky829@gmail.com>
2023-10-11 16:02:32 +0500

    checkout page body added

---
+++ src/Pages/Checkout/Checkout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Checkout = () => {
+  return (
+    <div className="checkout">
+        <div className="container">
+            <div className="checkout__wrapper">
+                <div className="checkout__form"></div>
+                <div className="checkout__total"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Checkout
